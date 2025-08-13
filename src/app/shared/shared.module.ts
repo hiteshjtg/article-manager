00cc4543b737053f12e3a0cc99f0fab0605ef3db
Hitesh Sharma <hitesh.sharma@joshtechnologygroup.com>
2025-08-13 17:28:33 +0530

    Added lazyloading to dashboard, added mat chip for tags input, created cards as button to view

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,27 @@ import { PaginatorComponent } from './paginator/paginator.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChipInputComponent } from './chip-input/chip-input.component';
+
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, inject} from '@angular/core';
+import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+
 
 @NgModule({
-  declarations: [PaginatorComponent, SpinnerComponent],
+  declarations: [PaginatorComponent, SpinnerComponent, ChipInputComponent],
   imports: [
     CommonModule,
     MatPaginatorModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatChipsModule,
   ],
-  exports: [PaginatorComponent, SpinnerComponent]
+  exports: [PaginatorComponent, SpinnerComponent, ChipInputComponent]
 })
 
 export class SharedModule {}
